9fafee0707fbede28f26892d0a961612dd867f2c
wiskonsin <luisriancho@gmail.com>
2018-12-06 16:42:02 +0100

    no message

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb://localhost:27017/node-login-passport-local' 
+        URI: 'mongodb://admin:Riancho86@ds125368.mlab.com:25368/user_aplicacion' 
     }
 };
