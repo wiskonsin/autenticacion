f757be7d7a1474a921e45f8c1b228c8084dcf0c2
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-12-08 01:41:21 +0100

    no message

--- public/sockets/main.js
+++ public/sockets/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000', {'forceNew': true}); // para conectarnos al servidor de sockets, con ello ya creamos la conexión
+var socket = io.connect('https://localhost:3000', {'forceNew': true}); // para conectarnos al servidor de sockets, con ello ya creamos la conexión
 
 socket.on('messages', function(data){
     console.log(data);
