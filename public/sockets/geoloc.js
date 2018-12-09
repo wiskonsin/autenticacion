39fce00ffc58882337633303febd398a545cb819
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-12-09 10:33:47 +0100

    no message

--- public/sockets/geoloc.js
+++ public/sockets/geoloc.js
@@ -27,7 +27,7 @@ function showPosition(position) {
     ; 
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
   var d = R * c; // Distance in km
-  x.innerHTML = "Latitud: " + la1 + 
+  x.innerHTML = "Latitud: " + lat1 + 
   "<br>Longitud: " + lon1 +
   "<br>Distancia al centro: " + d + " km";
 }
