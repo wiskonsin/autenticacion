9264a968db1a6d00d459e05935b601c5761c4be3
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-12-09 10:18:21 +0100

    no message

--- public/sockets/geoloc.js
+++ public/sockets/geoloc.js
@@ -4,19 +4,28 @@ var boton = document.getElementById("geoloc");
 boton.addEventListener("click",getLocation);
 
 function getLocation() {
-    console.log("geolocation");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-    console.log("geosi");
   } else { 
     x.innerHTML = "Geolocation is not supported by this browser.";
-    console.log("geono");
-
   }
 }
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+  var latitud = position.coords.latitude;
+  var longitud = position.coords.longitude;
+  var latitudCentro = 43.462391;
+  var longitudCentro = -3.809801;
+  var radioTierra = 6378;
+  var deltaLat = (latitudCentro - latitud)*Math.PI/180;
+  var deltaLon = (longitudCentro - longitud)*Math.PI/180;
+  var distancia = (Math.sin(deltaLat/2))*(Math.sin(longitudCentro/2))+
+  (Math.cos(latitud))*(Math.cos(latitudCentro))*
+  (Math.sin(deltaLon/2))*(Math.sin(deltaLon/2));
+  distancia = 2*Math.atan2(Math.sqrt(distancia),Math,sqrt(1-distancia));
+  distancia = radioTierra*distancia;
+  x.innerHTML = "Latitude: " + latitud + 
+  "<br>Longitude: " + longitud +
+  "<br>Distancia al centro: " + distancia;
 }
