893d253fcf75b326b96a78db3a6fd855721a41e6
Luis Alberto Riancho Martín <luisriancho@gmail.com>
2018-12-09 10:25:20 +0100

    no message

--- public/sockets/geoloc.js
+++ public/sockets/geoloc.js
@@ -20,6 +20,10 @@ function showPosition(position) {
   var radioTierra = 6378;
   var deltaLat = (latitudCentro - latitud)*Math.PI/180;
   var deltaLon = (longitudCentro - longitud)*Math.PI/180;
+  latitud = latitud*Math.PI/180;
+  longitud = longitud*Math.PI/180;
+  latitudCentro = latitudCentro*Math.PI/180;
+  longitudCentro = longitudCentro*Math.PI/180;
   var distancia = (Math.sin(deltaLat/2))*(Math.sin(longitudCentro/2))+
   (Math.cos(latitud))*(Math.cos(latitudCentro))*
   (Math.sin(deltaLon/2))*(Math.sin(deltaLon/2));
